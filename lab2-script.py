e7c3c904e8118086bac792ca2e2f3dd47aa94f52
Pramish pd Shrestha <156696133+PramishpdShrestha@users.noreply.github.com>
2024-01-24 10:58:21 -0500

    step 2 completed
    completed step 2

---
+++ lab2-script.py
@@ -0,0 +1,43 @@
+def main():
+
+    # TODO: Step 2 - Create a complex data structure
+    about_me = {
+        "full_name": "Pramish Prasad Shrestha",
+        "student_id": 10319935,
+        "pizza_toppings": ["BACON","ONION","PEPPERS"],
+        "movies": [
+            {
+                "title": "3-idiots",
+                "genre": "comedy"
+            },
+            {
+                "title": "intersteller",
+                "genre": "sci-fi"
+            },
+        ]
+    }
+
+    # TODO: Step 3 - Add another movie to the data structure
+    
+# TODO: Step 4 - Function that prints student name and ID	
+def print_student_name_and_id(about_me):
+    return
+    
+# TODO: Step 5 - Function that adds pizza toppings to data structure
+def add_pizza_toppings(about_me, toppings):
+    return
+
+# TODO: Step 6 - Function that prints bullet list of pizza toppings
+def print_pizza_toppings(about_me):
+    return
+
+# TODO: Step 7 - Function that prints comma-separated list of movie genres
+def print_movie_genres(about_me):
+    return 
+
+# TODO: Step 8 - Function that prints comma-separated list of movie titles
+def print_movie_titles(movie_list):
+    return
+    
+if __name__ == '__main__':
+    main()
